f67a2e13e859118577782737dc47229d57aa0791
Lester Lacson <iamjohnlacson@gmail.com>
2021-12-29 03:28:39 +0800

    Changed: App name to 'Wise Planner'

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         transition={{ type: "spring", duration: 0.5 }}
         whileHover={{ scale: 1.1 }}
       >
-        Todo Application
+        Wise Planner
       </motion.h1>
       <motion.div
         initial={{ y: 1000 }}
